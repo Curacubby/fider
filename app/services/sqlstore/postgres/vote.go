44f1ab984d89c8a820e3cc943fcfc435cd430ef2
blouisdiddy <meckler@gmail.com>
2020-04-26 00:51:31 -0700

    Update vote.go

--- app/services/sqlstore/postgres/vote.go
+++ app/services/sqlstore/postgres/vote.go
@@ -48,7 +48,7 @@ func addVote(ctx context.Context, c *cmd.AddVote) error {
 			tenant.ID, c.User.ID, c.Post.ID, time.Now(),
 		)
 
-		_, err := trx.Execute(
+		_, err = trx.Execute(
 			`INSERT INTO post_votes (tenant_id, user_id, post_id, created_at) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING;`,
 			tenant.ID, c.User.ID, c.Post.ID, time.Now(),
 		)
